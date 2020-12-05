111b9e45f797ff3dbac5dfc3565a0f627647f7ba
znaQ <znaQ>
2020-12-06 00:05:52 +0100

    askdj

--- charly/main.py
+++ charly/main.py
@@ -2,10 +2,9 @@ import logging
 from charly.modules.APICaller import APIHandler
 from charly.modules.DBConnector import DBConnector
 
-#create API Caller
+
 api = APIHandler()
 db = DBConnector()
 
-#do things
 playerList = api.call()
 db.write(playerList)
